1d0d50de41b9d2d29479938b70f06f70ecf0951d
yqzheng <yqzheng7@gmail.com>
2022-10-24 22:45:55 +0800

    Git test, second commit.

--- src/main/java/com/atguigu/github/test/GithubTest.java
+++ src/main/java/com/atguigu/github/test/GithubTest.java
@@ -3,6 +3,7 @@ package com.atguigu.github.test;
 public class GithubTest {
     public static void main(String[] args) {
         int i = 1;
+        i = 2;
         System.out.println("i = " + i);
     }
 }
