d80b58a14c9c01d126b9df7fe80c6cf5d6dee616
yqzheng <yqzheng7@gmail.com>
2022-10-24 23:14:38 +0800

    Git test, third commit.

--- src/main/java/com/atguigu/github/test/GithubTest.java
+++ src/main/java/com/atguigu/github/test/GithubTest.java
@@ -5,6 +5,7 @@ public class GithubTest {
         int i = 1;
         i = 2;
         i = 3; //update via hot-fix
+        i = 4; //update via master
         System.out.println("i = " + i);
     }
 }
