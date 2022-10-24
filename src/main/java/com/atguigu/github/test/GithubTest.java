0da9bc18d86decd47e297ed387bf7f97a1315493
yqzheng <yqzheng7@gmail.com>
2022-10-24 23:15:10 +0800

    Git test, hot-fix second commit.

--- src/main/java/com/atguigu/github/test/GithubTest.java
+++ src/main/java/com/atguigu/github/test/GithubTest.java
@@ -5,6 +5,7 @@ public class GithubTest {
         int i = 1;
         i = 2;
         i = 3; //update via hot-fix
+        i = 5; //update via hot-fix
         System.out.println("i = " + i);
     }
 }
