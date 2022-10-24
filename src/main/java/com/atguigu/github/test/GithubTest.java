05a2af8dc2bbec857ef32255b1d8864ad0de894b
yqzheng7 <116557241+yqzheng7@users.noreply.github.com>
2022-10-24 23:36:23 +0800

    Update test via Github

--- src/main/java/com/atguigu/github/test/GithubTest.java
+++ src/main/java/com/atguigu/github/test/GithubTest.java
@@ -8,6 +8,7 @@ public class GithubTest {
         i = 4; //update via master
         i = 5; //update via hot-fix
         i = 6; //push to GitHub
+        i = 7; //update via GitHub
         System.out.println("i = " + i);
     }
 }
