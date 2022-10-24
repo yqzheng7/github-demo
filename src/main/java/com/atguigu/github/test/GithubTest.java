dd02b8a2267620606fc5e006966195f9245bb709
yqzheng <yqzheng7@gmail.com>
2022-10-24 22:49:50 +0800

    Git test, hot-fix first commit.

--- src/main/java/com/atguigu/github/test/GithubTest.java
+++ src/main/java/com/atguigu/github/test/GithubTest.java
@@ -4,6 +4,7 @@ public class GithubTest {
     public static void main(String[] args) {
         int i = 1;
         i = 2;
+        i = 3; //update via hot-fix
         System.out.println("i = " + i);
     }
 }
